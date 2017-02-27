7e35082ee29e23084eeef77ff90b6d405795a9fe
Dshinji137 <hd2358@columbia.edu>
2017-02-27 00:56:30 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,179 +1,115 @@
 //
 //  ViewController.swift
-//  QuickDial
+//  What is the number II
 //
-//  Created by Hengzhi Ding on 17/2/22.
+//  Created by Hengzhi Ding on 17/2/21.
 //  Copyright © 2017年 Hengzhi Ding. All rights reserved.
 //
 
 import UIKit
 
-var colorArray:[UIColor]!
-var selectNumber:Int = -1
-var nameArray = ["","","","",""]
-var phoneArray = ["","","","",""]
-var isSetting = false
-var settingName:String?
-var settingPhone:String?
-
 class ViewController: UIViewController {
 
-    @IBOutlet weak var button1: BigLabelButton!
-    @IBOutlet weak var button2: BigLabelButton!
-    @IBOutlet weak var button3: BigLabelButton!
-    @IBOutlet weak var button4: BigLabelButton!
-    @IBOutlet weak var button5: BigLabelButton!
+    var A = 0
+    var B = 0
+    var guessTime = 0
+    let maxTime = 10
+    var ans:[Int] = [0,0,0,0]
+    var isOver = true
     
-    var buttonArray:[BigLabelButton]!
-    var viewWidth:CGFloat!
-    var viewHeight:CGFloat!
-    //var colorArray:[UIColor]!
-    var colorPressArray:[UIColor]!
+    @IBOutlet weak var myTitle: UILabel!
+    @IBOutlet weak var Judge: UILabel!
+    @IBOutlet weak var guessResult: UILabel!
+    @IBOutlet weak var inputNumber: UITextField!
     
-    // button Pressed, exert
-    @IBAction func buttonTouched(sender: BigLabelButton) {
-        // get which button pressed.
-        let buttonNumber = sender.tag - 100
-        buttonArray[buttonNumber].backgroundColor = colorPressArray[buttonNumber]
-        buttonArray[buttonNumber].bigLabel.textColor = colorPressArray[5]
-        
+    @IBAction func Reset(sender: UIButton) {
+        ans = distinctNumber()
+        isOver = false
+        guessTime = 0
+        Judge.hidden = true
+        guessResult.hidden = true
     }
-    
-    // button touch up inside, exert
-    @IBAction func pressEnded(sender: BigLabelButton) {
-        // get which button pressed.
-        let buttonNumber = sender.tag - 100
-        buttonArray[buttonNumber].backgroundColor = colorArray[buttonNumber]
-        buttonArray[buttonNumber].bigLabel.textColor = colorArray[5]
-        let thisName:String? = nameArray[buttonNumber]
-        let thisNumber:String? = phoneArray[buttonNumber]
-        // look if can dial
-        if thisName != nil {
-            if UIApplication.sharedApplication().canOpenURL(NSURL(string: "tel://\(thisNumber!)")!) == true {
-                let url = NSURL(string: "tell://\(thisNumber!)")
-                UIApplication.sharedApplication().openURL(url!)
-                
+    @IBAction func myGuess(sender: UIButton) {
+        if !isOver {
+            let input = inputNumber.text!
+            let len = input.characters.count
+            inputNumber.text = ""
+            if(len != 4) {
+                guessResult.hidden = false
+                guessResult.text = "Please guess a 4-digit number!"
             }
-        }
-        
-    }
-    // button touch up outside, exert (maybe to deal with wrong operation?)
-    @IBAction func outsideEnded(sender: BigLabelButton) {
-        // get which button pressed.
-        let buttonNumber = sender.tag - 100
-        buttonArray[buttonNumber].backgroundColor = colorArray[buttonNumber]
-        buttonArray[buttonNumber].bigLabel.textColor = colorArray[5]
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        isSetting = false
-        settingName = nil
-        settingPhone = nil
-        for var i = 0; i < buttonArray.count; i++ {
-            buttonArray[i].backgroundColor = colorArray[i]
-            buttonArray[i].bigLabel.textColor = colorArray[5]
-        }
-        
-        var isEmpty = true
-        for var i = 0; i < 5; i++ {
-            if nameArray[i] != "" {
-                isEmpty = false
-            }
-        }
-        if isEmpty == true {
-            for var i = 0; i < 5; i++ {
-                buttonArray[i].bigLabel.text = ""
-                buttonArray[i].bigLabel.hidden = true
-            }
-            buttonArray[1].bigLabel.text = "Long Press"
-            buttonArray[1].bigLabel.hidden = false
-        }
-        else {
-            for var i = 0; i < 5; i++ {
-                if nameArray[i] != "" {
-                    buttonArray[i].bigLabel.text = nameArray[i]
-                    buttonArray[i].bigLabel.hidden = false
+            else {
+                guessTime++
+                var guess:[Character] = [Character](input.characters)
+                for(var i = 0; i < 4; i++) {
+                    for(var j = 0; j < 4; j++) {
+                        if let num = Int(String(guess[i])) {
+                            if(num == ans[j]) {
+                                if(i == j) {
+                                    A++
+                                }
+                                else {
+                                    B++
+                                }
+                            }
+                        }
+                    }
+                }
+                if(A == 4) {
+                    guessResult.hidden = false
+                    guessResult.text = "Congratulations, you win! Press Reset to play again"
+                    Judge.hidden = false
+                    Judge.text = "\(A)A\(B)B                    \(guess[0])\(guess[1])\(guess[2])\(guess[3])"
+                    isOver = true
+                }
+                else if(guessTime < 10) {
+                    guessResult.hidden = false
+                    guessResult.text = "Not right,\(maxTime-guessTime) chances remaining"
+                    Judge.hidden = false
+                    Judge.text = "\(A)A\(B)B                    \(guess[0])\(guess[1])\(guess[2])\(guess[3])"
+                    A = 0
+                    B = 0
                 }
                 else {
-                    buttonArray[i].bigLabel.hidden = true
+                    guessResult.hidden = false
+                    guessResult.text = "Sorry, you lose! Press Reset to play again"
+                    isOver = true
                 }
             }
         }
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        viewWidth = view.frame.size.width
-        viewHeight = view.frame.size.height
-        buttonArray = [button1,button2,button3,button4,button5]
-        for var i = 0; i < buttonArray.count; i++ {
-            buttonArray[i].bigLabel.frame = CGRectMake(28, 0, viewWidth+100, viewHeight/5)
-            buttonArray[i].bigLabel.font = UIFont(name:"Arial-Bold",size:80*(viewHeight/667))
-            // add long press recognizer to each button
-            // ":" means there are some parameters in function "LongPress"
-            let longPress = UILongPressGestureRecognizer(target: self, action: Selector("LongPress:"))
-            longPress.minimumPressDuration = 1.0
-            buttonArray[i].addGestureRecognizer(longPress)
-        }
-        colorArray = [
-            UIColor(red: 232/255,green: 88/255, blue: 32/255, alpha:1),
-            UIColor(red: 250/255,green: 168/255, blue: 8/255, alpha:1),
-            UIColor(red: 2/255,green: 160/255, blue: 131/255, alpha:1),
-            UIColor(red: 9/255,green: 171/255, blue: 219/255, alpha:1),
-            UIColor(red: 3/255,green: 127/255, blue: 208/255, alpha:1),
-            UIColor.whiteColor()
-        ]
-        colorPressArray = [
-            UIColor(red: 202/255,green: 38/255, blue: 1/255, alpha:1),
-            UIColor(red: 220/255,green: 138/255, blue: 0/255, alpha:1),
-            UIColor(red: 0/255,green: 130/255, blue: 101/255, alpha:1),
-            UIColor(red: 0/255,green: 141/255, blue: 189/255, alpha:1),
-            UIColor(red: 0/255,green: 97/255, blue: 178/255, alpha:1),
-            UIColor.grayColor()
-        ]
-        
-        if NSUserDefaults.standardUserDefaults().objectForKey("contactName") != nil {
-            nameArray.removeAll(keepCapacity: true)
-            nameArray = NSUserDefaults.standardUserDefaults().objectForKey("contactName") as! Array
-        }
-        if NSUserDefaults.standardUserDefaults().objectForKey("contactPhone") != nil {
-            nameArray.removeAll(keepCapacity: true)
-            phoneArray = NSUserDefaults.standardUserDefaults().objectForKey("contactPhone") as! Array
-        }
-    }
-    
-    func LongPress(sender: UILongPressGestureRecognizer) {
-        if sender.state == .Began {
-            selectNumber = sender.view!.tag - 100
-            if nameArray[selectNumber] != "" {
-                self.performSegueWithIdentifier("showChooseDelete", sender: nil)
-            }
-            else {
-                self.performSegueWithIdentifier("showSettingName", sender: nil)
-            }
-        }
+        inputNumber.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     override func prefersStatusBarHidden() -> Bool {
+        // eliminate time status, etc.
         return true
     }
-}
-
-class BigLabelButton: UIButton {
-    let bigLabel = UILabel()
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        bigLabel.text = "Long Press"
-        bigLabel.textColor = UIColor.whiteColor()
-        bigLabel.shadowColor = UIColor.blackColor()
-        self.addSubview(bigLabel)
+    
+    func distinctNumber()->[Int] {
+        var ans = [0,0,0,0]
+        var ans1 = Int(arc4random_uniform(10))
+        var ans2 = Int(arc4random_uniform(10))
+        var ans3 = Int(arc4random_uniform(10))
+        var ans4 = Int(arc4random_uniform(10))
+        while(ans1==ans2 || ans1==ans3 || ans1==ans4 || ans2==ans3 || ans2==ans4 || ans3==ans4) {
+            ans1 = Int(arc4random_uniform(10))
+            ans2 = Int(arc4random_uniform(10))
+            ans3 = Int(arc4random_uniform(10))
+            ans4 = Int(arc4random_uniform(10))
+        }
+        ans = [ans1,ans2,ans3,ans4]
+        return ans
     }
+
+
+
 }
 
